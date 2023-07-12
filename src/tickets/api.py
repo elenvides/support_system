938bf23432ccc1d763eb91e781fc20c97b7967fc
elenvides <35038295+elenvides@users.noreply.github.com>
2023-07-13 00:43:44 +0300

    tickets CRUD (#9)
    * tickets CRUD

--- src/tickets/api.py
+++ src/tickets/api.py
@@ -1,11 +1,49 @@
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from tickets.models import Ticket
-from tickets.serializers import TicketSerializer
+from tickets.permissions import (
+    CanTakeTicket,
+    IsOwner,
+    RoleIsAdmin,
+    RoleIsManager,
+    RoleIsUser,
+)
+from tickets.serializers import TicketSerializer, TicketTakeSerializer
 
 
 class TicketAPIViewSet(ModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
-    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action == "list":
+            permission_classes = [RoleIsAdmin | RoleIsManager | RoleIsUser]
+        elif self.action == "create":
+            permission_classes = [RoleIsUser]
+        elif self.action == "retrieve":
+            permission_classes = [IsOwner | RoleIsAdmin | RoleIsManager]
+        elif self.action == "update":
+            permission_classes = [RoleIsAdmin | RoleIsManager]
+        elif self.action == "destroy":
+            permission_classes = [RoleIsAdmin | RoleIsManager]
+        elif self.action == "take":
+            permission_classes = [CanTakeTicket]
+        else:
+            permission_classes = []
+
+        return [permission() for permission in permission_classes]
+
+    @action(detail=True, methods=["post"])
+    def take(self, request, pk):
+        ticket = self.get_object()
+
+        serializer = TicketTakeSerializer(data={"manager_id": request.user.id})
+        serializer.is_valid()
+        ticket = serializer.assign(ticket)
+
+        return Response(TicketSerializer(ticket).data)
--- src/tickets/serializers.py
+++ src/tickets/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from tickets.models import Ticket
 
@@ -8,5 +9,25 @@ class TicketSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Ticket
-        fields = ["id", "title", "text", "visibility", "status", "user"]
-        read_only_fields = ["visibility"]
+        fields = ["id", "title", "text", "visibility", "status", "user", "manager"]
+        read_only_fields = ["visibility", "manager"]
+
+
+class TicketTakeSerializer(serializers.Serializer):
+    manager_id = serializers.IntegerField()
+
+    def validate_manager_id(self, manager_id):
+        return manager_id
+
+    def assign(self, ticket: Ticket) -> Ticket:
+        manager_id = self.validated_data["manager_id"]
+        tickets_count = Ticket.objects.filter(manager_id=manager_id).count()
+        if tickets_count >= 10:
+            raise ValidationError(
+                {"error": "A manager can be assigned to 10 tickets maximum"}
+            )
+
+        ticket.manager_id = self.validated_data["manager_id"]
+        ticket.save()
+
+        return ticket
